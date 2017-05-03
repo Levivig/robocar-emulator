6319f9bccac24339f5fb4d403edccda0e62e66f2
levivig <levivig@gmail.com>
2017-05-03 16:19:50 +0200

    Fixes

--- justine/rcemu/src/myshmclient.cpp
+++ justine/rcemu/src/myshmclient.cpp
@@ -310,7 +310,6 @@ void justine::sampleclient::MyShmClient::start10 ( boost::asio::io_service& io_s
 	std::vector<unsigned long int> schoolNodes = 
 	{422987265u, 1370021774u, 1326539937u, 343569186u, 1337631544u, 2936612672u, 267389855u, 1336963989u, 2924567329u, 1492882533u };
 
-
 	std::this_thread::sleep_for ( std::chrono::milliseconds ( 200 ) );
 
 	for ( auto cop:cops )
@@ -324,14 +323,14 @@ void justine::sampleclient::MyShmClient::start10 ( boost::asio::io_service& io_s
 				}
 		);
 
-/*
+		/*
 			gngstrs = gangsters ( socket, cop, t );
 
 			if ( gngstrs.size() > 0 )
 				g = gngstrs[0].to;
 			else
 				g = 0;
-			*/			
+		*/			
 
 		g = schoolNodes[0];
 
@@ -349,7 +348,6 @@ void justine::sampleclient::MyShmClient::start10 ( boost::asio::io_service& io_s
 
 		if( !(schoolNodes.empty()) )
 			schoolNodes.erase(schoolNodes.begin() + 0);
-
+		
 	}
 }
-
